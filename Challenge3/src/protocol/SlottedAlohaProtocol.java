3b0c0df75dc5e8572e8de9351b28cb9f47d43646
Bob Rubbens <bobrubbens@gmail.com>
2014-02-20 09:55:16 +0100

    Slotted Aloha finished

--- Challenge3/src/protocol/SlottedAlohaProtocol.java
+++ Challenge3/src/protocol/SlottedAlohaProtocol.java
@@ -1,15 +1,36 @@
 package protocol;
 
+import java.util.Random;
+
 public class SlottedAlohaProtocol implements IMACProtocol {
 
 	public SlottedAlohaProtocol() {
 		// TODO Auto-generated constructor stub
+		myGen = new Random();
 	}
+	
+	private Random myGen;
+	private int p = 5;
 
 	@Override
 	public TransmissionInfo TimeslotAvailable(MediumState previousMediumState,
 			int controlInformation, int localQueueLength) {
-		return new TransmissionInfo(TransmissionType.Data, 0);
+		
+		// No data to send, just be quiet
+		if (localQueueLength == 0) {
+			return new TransmissionInfo(TransmissionType.Silent, 0);
+		}
+		
+		if (previousMediumState == MediumState.Collision) {
+			// Random
+			if (myGen.nextInt(p) == 0) {
+				return new TransmissionInfo(TransmissionType.Data, 0);
+			} else {
+				return new TransmissionInfo(TransmissionType.Silent, 0);
+			}
+		} else {
+			return new TransmissionInfo(TransmissionType.Data, 0);
+		}
 	}
 
 }
